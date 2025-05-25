aeb8ffd6641a238a16d471248c440cb3735c8973
Daniil <rychenkov16@yandex.ru>
2025-05-25 19:49:55 +0300

    1

--- BlogApp/DAL/BlogDbContext.cs
+++ BlogApp/DAL/BlogDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using BlogApp.DAL.Models;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace BlogApp.DAL
 {
-    public class BlogDbContext : DbContext
+    public class BlogDbContext : IdentityDbContext<User, Role, string>
     {
         public DbSet<Post> Posts { get; set; }
         public DbSet<Tag> Tags { get; set; }
@@ -12,7 +13,7 @@ namespace BlogApp.DAL
 
         public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
         {
-            Database.EnsureCreated();
+            Database.Migrate();
         }
 
     }
