66c491466e14411438192a69d7706f84eacec8a3
Daniil <rychenkov16@yandex.ru>
2025-05-25 16:16:04 +0300

    add interfaces for repositories and repositories

---
+++ BlogApp/DAL/Repositories/IRepositories/ITagRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlogApp.DAL.Models;
+
+namespace BlogApp.DAL.Repositories.IRepositories
+{
+    public interface ITagRepository
+    {
+        List<Tag> GetAllTags();
+        Tag GetTag(Guid id);
+        Task AddTag(Tag tag);
+        Task UpdateTag(Tag tag);
+        Task RemoveTag(Guid id);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ BlogApp/DAL/Repositories/TagRepository.cs
@@ -0,0 +1,52 @@
+﻿using BlogApp.DAL.Models;
+using BlogApp.DAL.Repositories.IRepositories;
+
+namespace BlogApp.DAL.Repositories
+{
+    public class TagRepository : ITagRepository
+    {
+        private Blog2DbContext _context;
+
+        public TagRepository(Blog2DbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Tag> GetAllTags()
+        {
+            return _context.Tags.ToList();
+        }
+
+        public Tag GetTag(Guid id)
+        {
+            return _context.Tags.FirstOrDefault(t => t.Id == id);
+        }
+
+        public async Task AddTag(Tag tag)
+        {
+            _context.Tags.Add(tag);
+            await SaveChangesAsync();
+        }
+
+        public async Task UpdateTag(Tag tag)
+        {
+            _context.Tags.Update(tag);
+            await SaveChangesAsync();
+        }
+
+        public async Task RemoveTag(Guid id)
+        {
+            _context.Tags.Remove(GetTag(id));
+            await SaveChangesAsync();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
