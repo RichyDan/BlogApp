8e877cbb0f662159fa5512f4f26fea8409fad5fe
Daniil <rychenkov16@yandex.ru>
2025-05-31 18:56:11 +0300

    add models of response

---
+++ API/DATA/Models/Response/Role.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API.DATA.Models.Response
+{
+    public class Role : IdentityRole
+    {
+        public int? SecurityLvl { get; set; } = null;
+    }
+}
---
+++ API/DATA/Models/Response/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DATA.Models.Response
+{
+    public class Tag
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = String.Empty;
+        public List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
