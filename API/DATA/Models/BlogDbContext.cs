9732a74f7b144c0429f0d6c4263523183295a8e6
Daniil <rychenkov16@yandex.ru>
2025-05-31 18:51:04 +0300

    add contex for DB

---
+++ API/DATA/Models/BlogDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace API.DATA.Models
+{
+    public class BlogDbContext : IdentityDbContext<User, Role, string>
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+            if (!Database.GetService<IRelationalDatabaseCreator>().Exists())
+            {
+                Database.EnsureCreated();
+            }
+            else
+            {
+                Database.Migrate();
+            }
+        }
+    }
+}
+}
